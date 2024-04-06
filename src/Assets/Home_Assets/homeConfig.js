2dea60cb9069f776a802758ed9b2f3d65a8b146b
Akash <akashbytes@gmail.com>
2024-04-06 16:33:17 +0530

    project image and hardcoded data..

---
+++ src/Assets/Home_Assets/homeConfig.js
@@ -0,0 +1,29 @@
+
+import img1 from "./resturent1.jpg"
+import img2 from "./resturent2.jpg"
+import img3 from "./resturent3.jpg"
+import img4 from "./resturent4.jpg"
+
+
+export const Home_Config = [
+    {
+        id:"H1",
+    img_Url :img1 ,
+    title : "The Golden Fork " ,
+} , 
+{
+    id:"H2",
+    img_Url :img2 ,
+    title : " The House of Food " ,
+} ,
+{
+    id:"H3",
+    img_Url : img3,
+    title : "The Harvest Table  " ,
+},
+{
+    id:"H4",
+    img_Url : img4 ,
+    title : "The 6th Street Grill  " ,
+}
+];
